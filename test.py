30f1ada324f7d223a40824be752a7199567d9c3d
Romain <74216407+spawnzii@users.noreply.github.com>
2025-06-23 14:55:57 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,26 @@
+import socket
+
+HOST = '0.0.0.0'
+PORT = 8888
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind((HOST, PORT))
+    server.listen(1)
+    print(f"Listening on port {PORT}...")
+    conn, addr = server.accept()
+    print(f"Connected by {addr}")
+
+    with conn:
+        while True:
+            cmd = input("Shell> ")
+            if cmd.lower() in ['exit', 'quit']:
+                conn.sendall(b'exit\n')
+                break
+            conn.sendall((cmd + '\n').encode('utf-8'))  # <<< envoie aussi \n
+            data = b""
+            while True:
+                chunk = conn.recv(1024)
+                data += chunk
+                if len(chunk) < 1024:
+                    break
+            print(data.decode('utf-8'))
